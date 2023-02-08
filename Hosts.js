fbd2818def2e94412b639aa5835211f80d55193f
Mystic-Mirage <mm@m10e.net>
2023-02-08 18:45:06 +0200

    Fix hosts sorting

--- Hosts.js
+++ Hosts.js
@@ -27,7 +27,7 @@ function getHosts(sheet) {
 
   hosts.sort(function (a, b) {
     if (a.timestamp < b.timestamp) return -1;
-    if (a.timestamp < b.timestamp) return 1;
+    if (a.timestamp > b.timestamp) return 1;
     return 0;
   });
 
