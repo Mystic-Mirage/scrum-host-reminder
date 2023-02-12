8068ab9930af5b2f433ec3bab8ef7c0cdda83a9b
Mystic-Mirage <mm@m10e.net>
2023-02-12 14:51:09 +0200

    Prettify Schedule.test.js

--- test/Schedule.test.js
+++ test/Schedule.test.js
@@ -1,19 +1,19 @@
-let assert = require("assert");
+const assert = require("assert");
 
-let itParam = require("mocha-param");
-let rewire = require("rewire");
+const itParam = require("mocha-param");
+const rewire = require("rewire");
 
 const ScheduleModule = rewire("../Schedule.js");
 /** @type {Class<Schedule>} */
 const Schedule = ScheduleModule.__get__("Schedule");
 const tzDate = ScheduleModule.__get__("tzDate");
 
-describe(tzDate.name, function () {
-  it("should be 01:00 (UTC) when 03:00 (Kyiv) in Winter", function () {
+describe(tzDate.name, () => {
+  it("should be 01:00 (UTC) when 03:00 (Kyiv) in Winter", () => {
     assert.deepStrictEqual(tzDate(new Date("2023-02-06 03:00"), "Europe/Kiev"), new Date("2023-02-06 01:00+00:00"));
   });
 
-  it("should be 00:00 (UTC) when 03:00 (Kyiv) in Summer", function () {
+  it("should be 00:00 (UTC) when 03:00 (Kyiv) in Summer", () => {
     assert.deepStrictEqual(tzDate(new Date("2023-07-17 03:00"), "Europe/Kiev"), new Date("2023-07-17 00:00+00:00"));
   });
 });
@@ -39,69 +39,69 @@ function getNextMeetingParam(desc, startPoint, now, expected) {
   }
 }
 
-let getNextMeetingParams = [
-  getNextMeetingParam(
-    "should be today if current time is before today's meeting time",
-    "2023-02-06 00:00",
-    "2023-02-06 01:23:45+00:00",
-    "2023-02-06 11:00+02:00",
-  ),
-  getNextMeetingParam(
-    "should be next day if current time is after today's meeting time",
-    "2023-02-06 00:00",
-    "2023-02-06 10:23:45+00:00",
-    "2023-02-07 11:00+02:00",
-  ),
-  getNextMeetingParam(
-    "should be next week if current time is before a weekend",
-    "2023-02-06 00:00",
-    "2023-02-10 09:00:01+00:00",
-    "2023-02-13 11:00+02:00",
-  ),
-  getNextMeetingParam(
-    "should be at the same time if current time is before daylight saving change",
-    "2023-02-06 00:00",
-    "2023-03-31 09:00:01+00:00",
-    "2023-04-03 11:00+03:00",
-  ),
-  getNextMeetingParam(
-    "should be next day if start point in the middle of a day and current time is before it",
-    "2023-02-06 12:00",
-    "2023-02-06 07:00+00:00",
-    "2023-02-07 11:00+02:00",
-  ),
-  getNextMeetingParam(
-    "should be next day if start point in the middle of a day and current time is after it",
-    "2023-02-06 12:00",
-    "2023-02-06 13:00+00:00",
-    "2023-02-07 11:00+02:00",
-  ),
-  getNextMeetingParam(
-    "should be today if start point in the middle of a day and current time is the next day after it before meeting time",
-    "2023-02-06 12:00",
-    "2023-02-07 07:00+00:00",
-    "2023-02-07 11:00+02:00",
-  ),
-  getNextMeetingParam(
-    "should be on start point if start point in the future and current time is before meeting time",
-    "2023-02-13 00:00",
-    "2023-02-06 08:32+00:00",
-    "2023-02-13 11:00+02:00",
-  ),
-  getNextMeetingParam(
-    "should be on start point if start point in the future and current time is after meeting time",
-    "2023-02-13 00:00",
-    "2023-02-06 12:48+00:00",
-    "2023-02-13 11:00+02:00",
-  ),
-]
-
 describe(Schedule.prototype.getNextMeeting.name, function () {
-  itParam("${value.desc}", getNextMeetingParams, function (value) {
+  const getNextMeetingParams = [
+    getNextMeetingParam(
+      "should be today if current time is before today's meeting time",
+      "2023-02-06 00:00",
+      "2023-02-06 01:23:45+00:00",
+      "2023-02-06 11:00+02:00",
+    ),
+    getNextMeetingParam(
+      "should be next day if current time is after today's meeting time",
+      "2023-02-06 00:00",
+      "2023-02-06 10:23:45+00:00",
+      "2023-02-07 11:00+02:00",
+    ),
+    getNextMeetingParam(
+      "should be next week if current time is before a weekend",
+      "2023-02-06 00:00",
+      "2023-02-10 09:00:01+00:00",
+      "2023-02-13 11:00+02:00",
+    ),
+    getNextMeetingParam(
+      "should be at the same time if current time is before daylight saving change",
+      "2023-02-06 00:00",
+      "2023-03-31 09:00:01+00:00",
+      "2023-04-03 11:00+03:00",
+    ),
+    getNextMeetingParam(
+      "should be next day if start point in the middle of a day and current time is before it",
+      "2023-02-06 12:00",
+      "2023-02-06 07:00+00:00",
+      "2023-02-07 11:00+02:00",
+    ),
+    getNextMeetingParam(
+      "should be next day if start point in the middle of a day and current time is after it",
+      "2023-02-06 12:00",
+      "2023-02-06 13:00+00:00",
+      "2023-02-07 11:00+02:00",
+    ),
+    getNextMeetingParam(
+      "should be today if start point in the middle of a day and current time is the next day after it before meeting time",
+      "2023-02-06 12:00",
+      "2023-02-07 07:00+00:00",
+      "2023-02-07 11:00+02:00",
+    ),
+    getNextMeetingParam(
+      "should be on start point if start point in the future and current time is before meeting time",
+      "2023-02-13 00:00",
+      "2023-02-06 08:32+00:00",
+      "2023-02-13 11:00+02:00",
+    ),
+    getNextMeetingParam(
+      "should be on start point if start point in the future and current time is after meeting time",
+      "2023-02-13 00:00",
+      "2023-02-06 12:48+00:00",
+      "2023-02-13 11:00+02:00",
+    ),
+  ];
+
+  itParam("${value.desc}", getNextMeetingParams, (value) => {
     ScheduleModule.__with__({
       "getNow": () => value.now,
       "Schedule.prototype.getScheduleData": () => value.scheduleData,
-    })(function () {
+    })(() => {
       assert.deepStrictEqual(new Schedule().getNextMeeting(), value.expected);
     });
   });
